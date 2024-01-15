625e4ab541442ea019df139afcc4dc333af24720
Karol Jabłoński <Karol.jablonski00@gmail.com>
2024-01-15 21:21:08 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Game_Menager_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Game_Menager_Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		public IActionResult Logowanie()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
