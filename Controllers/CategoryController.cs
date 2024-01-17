facf116f3a814dcb7b93495786bf2289a9f210cc
Karol Jabłoński <Karol.jablonski00@gmail.com>
2024-01-17 16:32:50 +0100

    Create CategoryController.cs

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Game_Menager_Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
