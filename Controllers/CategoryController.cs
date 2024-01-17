c8d6039a136c942516e2b3581bd78561e82599d8
Karol Jabłoński <Karol.jablonski00@gmail.com>
2024-01-17 20:40:28 +0100

    Dodałem wyświetlanie sql w categories

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Game_Menager_Web.Data;
+using Game_Menager_Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Game_Menager_Web.Controllers
 {
     public class CategoryController : Controller
     {
+        
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            // dodaje naszą liste do strony
+            List<Category> objCategoryList = _db.Categories.ToList();
             // przekierowuje na stronę do logowania
-            return View();
+            return View(objCategoryList);
         }
     }
 }
