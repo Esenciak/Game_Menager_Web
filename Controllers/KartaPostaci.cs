d7ffbf7919f82345f6725bf033760f8040e2bafe
Karol Jabłoński <Karol.jablonski00@gmail.com>
2024-01-19 18:23:18 +0100

    upd

---
+++ Controllers/KartaPostaci.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Game_Menager_Web.Controllers
+{
+    public class KartaPostaci : Controller
+    {
+        // GET: KartaPostaci
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: KartaPostaci/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: KartaPostaci/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: KartaPostaci/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: KartaPostaci/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: KartaPostaci/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: KartaPostaci/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: KartaPostaci/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Data/HeroesDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Game_Menager_Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Game_Menager_Web.Data
+{
+    public class HeroesDbContext
+    {
+
+        public HeroesDbContext(DbContextOptions<HeroesDbContext> opcje) : base(opcje)
+        {
+
+        }
+
+        // tworzy Db Hero w SQL
+        public DbSet<Hero> Heroes { get; set; }
+
+        // dodaje dane do bazy sql Heroes
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Hero>().HasData(
+                new Hero
+                {
+                    Id = 1,
+                    Name = "Legolas",
+                    Description = "elf pochodzący ze szczepu Sindarów. W Trzeciej Erze był członkiem Drużyny Pierścienia.",
+                    Age = 1200,
+                    Race = "Elf",
+                    HP = 70,
+                    Speed = 120,
+                    Gold = 20,
+                    Strenght = 40,
+                    Dexterity = 100,
+                    Mana = 20,
+                    Male = 0,
+                    Weapon = "Bow",
+                    Skill = "Ciche kroki",
+                    SkillRank = 5,
+                    SkillDescription = "Postać porusza się bezszelestnie"
+                }
+                );
+        }
+
+
+
+    }
+}
