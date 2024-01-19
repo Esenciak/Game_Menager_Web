f094ad7823e095a554daa297c4ace86fd62d5200
Karol Jabłoński <Karol.jablonski00@gmail.com>
2024-01-19 18:37:44 +0100

    upd

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -28,8 +28,32 @@ namespace Game_Menager_Web.Data
                 );
         }
 
-        
-            
+        // tworzy Db Hero w SQL
+        public DbSet<Hero> Heroes { get; set; }
+
+        /* dodaje dane do bazy sql Heroes
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Hero>().HasData(
+                new Hero { Id = 1, Name = "Legolas",
+                    Description = "elf pochodzący ze szczepu Sindarów. W Trzeciej Erze był członkiem Drużyny Pierścienia.",
+                    Age = 1200,
+                    Race = "Elf",
+                    HP = 70,
+                    Speed = 120,
+                    Gold = 20,
+                    Strenght = 40,
+                    Dexterity = 100,
+                    Mana = 20,
+                    Male = 0,
+                    Weapon = "Bow",
+                    Skill = "Ciche kroki",
+                    SkillRank = 5,
+                    SkillDescription = "Postać porusza się bezszelestnie"
+                }
+                );
+        }
+            */
 
     }
 }
