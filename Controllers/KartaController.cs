cdfda36e2da22b9fb610654ee04f854998a2cdd2
Karol Jabłoński <Karol.jablonski00@gmail.com>
2024-01-20 18:44:42 +0100

    zły upd

---
+++ Controllers/KartaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Game_Menager_Web.Data;
+using System.Linq;
+
+namespace Game_Menager_Web.Controllers
+{
+	public class KartaPosController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+
+		public KartaPosController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public IActionResult KartaPos()
+		{
+			var heroesList = _db.Heroes.ToList();
+			return View(heroesList);
+		}
+	}
+}
