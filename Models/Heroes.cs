c47e02c2309c94aa02e2fd52faf1575b353a18b4
Karol Jabłoński <Karol.jablonski00@gmail.com>
2024-01-20 13:38:47 +0100

    Update Heroes.cs

--- Models/Heroes.cs
+++ Models/Heroes.cs
@@ -10,57 +10,57 @@ namespace Game_Menager_Web.Models
         [Required(ErrorMessage ="Nazwa nie może być pusta")]
 		[MaxLength(30)]
 		[DisplayName("Imie")]
-		public string Name { get; set; } = string.Empty;
+		public string Name { get; set; }
 		[Required]
 		[DisplayName("Opis")]
-		public string Description { get; set; } = "test";
+		public string Description { get; set; }
 		[Required]
 		[Range(1, 10000, ErrorMessage = "Numer musi być pomiędzy 1-10000")]
 		[DisplayName("Wiek")]
-		public int Age { get; set; } = 20;
+		public int Age { get; set; }
 		[Required]
 		[MaxLength(30)]
 		[DisplayName("Rasa")]
-		public string Race { get; set; } = "test";
+		public string Race { get; set; }
 		[Required]
 		[Range(1, 100, ErrorMessage = "Numer musi być pomiędzy 1-100")]
-		public int HP { get; set; } = 10;
+		public int HP { get; set; }
 		[Required]
 		[Range(1, 100, ErrorMessage = "Numer musi być pomiędzy 1-100")]
 		[DisplayName("Szybkość")]
-		public int Speed { get; set; } = 10;
+		public int Speed { get; set; }
 		[Required]
 		[DisplayName("Złoto")]
-		public int Gold { get; set; } = 10;
+		public int Gold { get; set; }
 		[Required]
 		[Range(1, 100, ErrorMessage = "Numer musi być pomiędzy 1-100")]
 		[DisplayName("Siła")]
-		public int Strenght { get; set; } = 10;
+		public int Strenght { get; set; }
 		[Required]
 		[Range(1, 100, ErrorMessage = "Numer musi być pomiędzy 1-100")]
 		[DisplayName("Zwinność")]
-		public int Dexterity { get; set; } = 10;
+		public int Dexterity { get; set; }
 		[Required]
 		[Range(0, 999, ErrorMessage = "Numer musi być pomiędzy 0-999")]
-		public int Mana { get; set; } = 10;
+		public int Mana { get; set; }
 		[Required]
 		[DisplayName("Płeć")]
-		public string Gender { get; set; } = "Male";
+		public string Gender { get; set; }
 		[Required]
 		[MaxLength(30)]
 		[DisplayName("Broń")]
-		public string Weapon { get; set; } = "test";
+		public string Weapon { get; set; }
 		[Required]
 		[MaxLength(40)]
 		[DisplayName("Umiejętność Główna")]
-		public string Skill { get; set; } = "test";
+		public string Skill { get; set; }
 		[Required]
 		[DisplayName("Poziom Głównej Umiejętności")]
-		public int SkillRank { get; set; } = 10;
+		public int SkillRank { get; set; }
 		[Required]
 		[MaxLength(500)]
 		[DisplayName("Opis Głównej umiejętności")]
-		public string SkillDescription { get; set; } = "test";
+		public string SkillDescription { get; set; }
 
 		public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
