d5094b24d3c0ba69fe6515916cd2f32d246749a7
Esenciak <karol.jablonski00@gmail.com>
2024-01-28 08:35:27 +0100

    .

--- Game_Menager.Models/obj/Debug/net8.0/Game_Menager.Models.AssemblyInfo.cs
+++ Game_Menager.Models/obj/Debug/net8.0/Game_Menager.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game_Menager.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf3bc507e6fa0a5488ea3b0135c2a2717ccfe7b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e986473c2a5b962a2a85800b670fd1d9c969343")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game_Menager.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game_Menager.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
