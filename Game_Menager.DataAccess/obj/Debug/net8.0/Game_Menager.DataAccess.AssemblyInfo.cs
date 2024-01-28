4a84abf7f93dfd36a76c13aba5f2df0efe504671
Esenciak <karol.jablonski00@gmail.com>
2024-01-28 08:45:42 +0100

    .

--- Game_Menager.DataAccess/obj/Debug/net8.0/Game_Menager.DataAccess.AssemblyInfo.cs
+++ Game_Menager.DataAccess/obj/Debug/net8.0/Game_Menager.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game_Menager.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6518495f28036c48b65a199cb1aacb0c66318e82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c0b9be285635113402edaeb855bab7844e8092d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game_Menager.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game_Menager.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
